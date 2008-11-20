e6fe65f5be093b39a955e89b7ba54bedd0956a9c
Matthias Georgi <matti.georgi@gmail.com>
2008-11-20 19:07:35 +0100

    clicking into the highlighted code now replace the pre element with a textarea.

--- assets/javascripts/coderay.js
+++ assets/javascripts/coderay.js
@@ -4,23 +4,25 @@
 	var text = '';
 	var rows = 0;
 
-	$('li', this).each(function() {
+	$('li', pre).each(function() {
 	    text += $(this).text() + "\n";
 	    rows += 1;
 	});
 
-	$(this).replaceWith('<textarea class="highlight" cols="80" rows="' + rows + ' ">' + text + '</textarea>');
+	$(pre).replaceWith('<textarea class="code" cols="80" rows="' + rows + ' ">' + text + '</textarea>');
 
-	$('textarea.highlight').blur(function() {
-	    $(this).replaceWith(pre);
-	    $(pre).click(function() {
-		convertToTextarea(this);
+	setTimeout(function() {
+	    $('textarea.code').blur(function() {
+		$(this).replaceWith(pre);
+		$(pre).click(function() {
+		    convertToTextarea(this);
+		});
 	    });
-	});
+	}, 100);
     }
 
 
-    $('pre.highlight').click(function() {
+    $('pre').click(function() {
 	convertToTextarea(this);
     });
 
