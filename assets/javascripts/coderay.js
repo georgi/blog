fee7d88bf86fe6a9e5d9dbc11f5421258e123fa7
Matthias Georgi <matti.georgi@gmail.com>
2008-12-15 17:31:24 +0100

    fixed coderay textarea script.

--- assets/javascripts/coderay.js
+++ assets/javascripts/coderay.js
@@ -2,8 +2,8 @@
 $('pre.highlight').each(function() {
     var pre = $(this);
     var text = $('li', pre).map(function() { return $(this).text(); }).get();
-    var textarea = $('<textarea cols="80" rows="' + text.length + ' ">' + text.join("\n") + '</textarea>');
-    var div = $('<div style="text-align:right"><a href="#">Toggle Code</a></div>').insertAfter(pre);
+    var textarea = $('<textarea class="code-area" cols="80" rows="' + text.length + ' ">' + text.join("\n") + '</textarea>');
+    var div = $('<div class="code-link"><a href="#">View plain</a></div>').insertBefore(pre);
     var toggle = false;
     $('a', div).click(function() {
 	toggle ? textarea.replaceWith(pre) : pre.replaceWith(textarea);
