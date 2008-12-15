8a2e4f6e95887c3807b03a4990aa94e1d311d690
Matthias Georgi <matti.georgi@gmail.com>
2008-12-15 14:48:39 +0100

    removed symlinks

--- assets/javascripts/coderay.js
+++ assets/javascripts/coderay.js
@@ -1,29 +1,13 @@
-(function() {
 
-    function convertToTextarea(pre) {
-	var text = '';
-	var rows = 0;
-
-	$('li', pre).each(function() {
-	    text += $(this).text() + "\n";
-	    rows += 1;
-	});
-
-	$(pre).replaceWith('<textarea class="code" cols="80" rows="' + rows + ' ">' + text + '</textarea>');
-
-	setTimeout(function() {
-	    $('textarea.code').blur(function() {
-		$(this).replaceWith(pre);
-		$(pre).click(function() {
-		    convertToTextarea(this);
-		});
-	    });
-	}, 1000);
-    }
-
-
-    $('pre.highlight').click(function() {
-	convertToTextarea(this);
+$('pre.highlight').each(function() {
+    var pre = $(this);
+    var text = $('li', pre).map(function() { return $(this).text(); }).get();
+    var textarea = $('<textarea cols="80" rows="' + text.length + ' ">' + text.join("\n") + '</textarea>');
+    var div = $('<div style="text-align:right"><a href="#">Toggle Code</a></div>').insertAfter(pre);
+    var toggle = false;
+    $('a', div).click(function() {
+	toggle ? textarea.replaceWith(pre) : pre.replaceWith(textarea);
+	toggle = !toggle;
+	return false;
     });
-
-})();
+});
