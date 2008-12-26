91bcaa87262a273264e1c3af51d2361b69702211
Matthias Georgi <matti.georgi@gmail.com>
2008-12-26 10:32:56 +0100

    fixed shockwave javascript

--- assets/javascripts/shockwaves.js
+++ assets/javascripts/shockwaves.js
@@ -1,6 +1,6 @@
 $(function() {
     $('.swfobject').each(function() {
-	swfobject.embedSWF('/shockwaves/' + this.id + '.swf', this.id, "400", "320", "9.0.0");
+	swfobject.embedSWF('/assets/shockwaves/' + this.id + '.swf', this.id, "400", "320", "9.0.0");
     });
 });
 
