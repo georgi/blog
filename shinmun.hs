34edd892a25d1623aed9dacfc6f5ebe4067ca00d
Matthias Georgi <matti.georgi@gmail.com>
2012-12-01 12:37:22 +0100

    resolved warnings

--- shinmun.hs
+++ shinmun.hs
@@ -1,35 +1,46 @@
 import Text.StringTemplate
 import Text.Sundown.Html.String
-import System.IO
 import System.Directory
 import System.Locale
-import Data.List
 import Data.List.Split
 import Data.Time.Format
 import Data.Time.Calendar
 
 data Post = Post { folder, file, text :: String };
 
+postTitle :: Post -> String
 postTitle post = head $ lines $ text post
+
+postName :: Post -> String
 postName  post = head $ splitOn "." $ file post
+
+postLink :: Post -> String
 postLink post = (folder post) ++ "/" ++ (postName post) ++ ".html"
+
+postDate :: Post -> String
 postDate post =
   let [year, month] = splitOn "/" (folder post)
       date = fromGregorian (read year) (read month) 1 in
     formatTime defaultTimeLocale "%d %b %y 00:00" date
+
+postHtml :: Post -> String
 postHtml  post = renderHtml (text post) allExtensions noHtmlModes True Nothing
+
+postBody :: Post -> [String]
 postBody  post = drop 3 (lines (text post))
+
+postDesc :: Post -> String
 postDesc  post = concat $ takeWhile (/= "") (postBody post)
 
-readPost :: String -> IO Post
+readPost :: FilePath -> IO Post
 readPost path = do
-  text <- readFile path
-  let [prefix, year, month, file] = splitOn "/" path in
-    return Post { folder = year ++ "/" ++ month, file = file, text = text }
+  s <- readFile path
+  let [_, year, month, filename] = splitOn "/" path in
+    return Post { folder = year ++ "/" ++ month, file = filename, text = s }
 
 renderTmpl :: String -> [(String, String)] -> IO String
-renderTmpl file variables = do
-  templateText <- readFile file
+renderTmpl filename variables = do
+  templateText <- readFile filename
   let template = newSTMP templateText in
     return $ toString $ setManyAttrib variables template
 
@@ -66,7 +77,7 @@ writeBlog :: IO ()
 writeBlog = do
   files <- findFiles "posts"
   posts <- mapM readPost files
-  mapM writePost posts
+  mapM_ writePost posts
   writeIndex $ reverse posts
   writeIndexRss $ reverse posts
 
@@ -81,9 +92,9 @@ findFiles path = do
       files <- mapM findFiles paths
       return $ concat files
   where
-    getEntries path = do
-      contents <- getDirectoryContents path
+    getEntries filepath = do
+      contents <- getDirectoryContents filepath
       return $ filter ((/= '.') . head) contents
 
+main :: IO ()
 main = writeBlog
-
